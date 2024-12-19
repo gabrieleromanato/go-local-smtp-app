d2301dfe46a08ee07f8af0b8e0aee63dafc4b17c
Gabriele Romanato <gabriele@gabrieleromanato.com>
2024-12-19 20:14:30 +0100

    Added logo

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { logout, isAuthenticated } from '@/api/auth';
+import { RouterLink } from 'vue-router';
 
 const isLogged = ref(isAuthenticated());
 
@@ -13,6 +14,11 @@ const handleLogout = () => {
 <template>
     <nav class="navigation">
         <div class="wrapper">
+            <div class="logo">
+                <RouterLink to="/">
+                    <span class="visually-hidden">Emails</span>
+                </RouterLink>
+            </div>
             <ul>
                 <li v-if="isLogged">
                     <button @click="handleLogout">Logout</button>
